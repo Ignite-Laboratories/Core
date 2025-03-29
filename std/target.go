9498cf1171a8983a08740fb274698eda9a6b7a32
Robert Petz <rpetz@ignitelabs.net>
2025-03-29 11:50:10 -0700

    Update target.go

--- std/target.go
+++ std/target.go
@@ -3,6 +3,8 @@ package std
 // TargetFunc functions return a pointer to a value.
 type TargetFunc[TValue any] func() *TValue
 
-func Target[TValue any](val *TValue) *TValue {
-	return val
+func Target[TValue any](val *TValue) TargetFunc[TValue] {
+	return func() *TValue {
+		return val
+	}
 }

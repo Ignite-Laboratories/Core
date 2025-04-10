01ac9ba8b3da5036e5e69c31e8ba6b4a9399b3bd
Robert Petz <rpetz@ignitelabs.net>
2025-04-10 12:55:47 -0700

    Created the "Synchro"

---
+++ std/synchro.go
@@ -0,0 +1,44 @@
+package std
+
+import "sync"
+
+// Synchro represents a way to synchronize data between two threads.
+//
+// The mechanic is simple - one thread creates a contextual "packet" of data to send to another thread
+// over a channel.  The other thread should handle messages on that channel and manipulate the packet
+// as desired.  The channel acts as a "bridge" between two spinning threads that need periodic
+// synchronization.
+//
+// To send data using a synchro, use:
+//
+//	msg = std.SynchroSend(bridge, message)
+//
+// To process data using a synchro bridge, use:
+//
+//	std.SynchroEngage(bridge, func(data) { ... })
+type Synchro[T any] struct {
+	sync.WaitGroup
+	Data *T
+}
+
+// SynchroSend sends the provided data over the bridge and waits for a result.
+func SynchroSend[T any](bridge chan *Synchro[T], message *T) *T {
+	synchro := &Synchro[T]{Data: message}
+	synchro.Add(1)
+	bridge <- synchro
+	synchro.Wait()
+	return synchro.Data
+}
+
+// SynchroEngage bridges the action against the provided channel and then calls Done() on the Synchro.
+func SynchroEngage[T any](bridge chan *Synchro[T], action func(*T)) {
+	for {
+		select {
+		case synchro := <-bridge:
+			action(synchro.Data)
+			synchro.Done()
+		default:
+			return
+		}
+	}
+}

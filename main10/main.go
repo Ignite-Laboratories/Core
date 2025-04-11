089d4eab168dfb038ccaa750f3d0fe58b1be4c9e
Robert Petz <rpetz@ignitelabs.net>
2025-04-10 19:54:28 -0700

    Update main.go

--- main10/main.go
+++ main10/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/ignite-laboratories/core"
-	"github.com/ignite-laboratories/core/temporal"
 	"github.com/ignite-laboratories/core/when"
 )
 
 func main() {
-	temporal.Loop(core.Impulse, when.Always, false, sample)
+	core.Impulse.Loop(sample, when.Always, false)
 	core.Impulse.Spark()
 }
 

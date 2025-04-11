d9ed79a5d0eaf99ecd3ed070a2299b67fc55756d
Robert Petz <rpetz@ignitelabs.net>
2025-04-10 18:51:44 -0700

    Update main.go

--- main8/main.go
+++ main8/main.go
@@ -16,12 +16,12 @@ func main() {
 
 	// Create a full-screen window
 	window, err := sdl.CreateWindow(
-		"SDL2 Full-Screen Example", // Window title
-		sdl.WINDOWPOS_CENTERED,     // Window position X
-		sdl.WINDOWPOS_CENTERED,     // Window position Y
-		1920,                       // Window width (ignored in full-screen mode)
-		1080,                       // Window height (ignored in full-screen mode)
-		sdl.WINDOW_FULLSCREEN,      // Full-screen mode flag
+		"SDL2 Full-Screen Example",    // Window title
+		sdl.WINDOWPOS_CENTERED,        // Window position X
+		sdl.WINDOWPOS_CENTERED,        // Window position Y
+		1920,                          // Window width (ignored in full-screen mode)
+		1080,                          // Window height (ignored in full-screen mode)
+		sdl.WINDOW_FULLSCREEN_DESKTOP, // Full-screen mode flag
 	)
 	if err != nil {
 		log.Fatalf("Failed to create window: %s\n", err)
@@ -30,9 +30,9 @@ func main() {
 	running := true
 
 	go func() {
-		time.Sleep(time.Second * 5)
+		fmt.Println("Exiting")
+		time.Sleep(time.Second * 10)
 		running = false
-		sdl.Quit()
 	}()
 
 	// Run the event loop to display the window and check for inputs
@@ -53,5 +53,5 @@ func main() {
 		sdl.Delay(16) // ~60 fps
 	}
 
-	fmt.Println("Exiting program...")
+	fmt.Println("Exit")
 }

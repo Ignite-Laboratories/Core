16ba6cba2bee49d32b816d7b364ce1f3d8975119
Robert Petz <rpetz@ignitelabs.net>
2025-05-07 13:50:13 -0700

    Added the std.FuzzyMap type

--- temporal/nexus.go
+++ temporal/nexus.go
@@ -1,15 +1,17 @@
 package temporal
 
+import "github.com/ignite-laboratories/core/std"
+
 func init() {
 	u := make(Universe)
 	Nexus = &u
 }
 
 // Universe is a collection of named worlds.
-type Universe map[string]World
+type Universe std.FuzzyMap[string, World]
 
-// World is a collection of named dimensions.
-type World map[string]*Dimension[any, any]
+// World is a collection of named dimensional slices.
+type World std.FuzzyMap[string, []*Dimension[any, any]]
 
 // Nexus is the global universe of all dimensions.
 var Nexus *Universe

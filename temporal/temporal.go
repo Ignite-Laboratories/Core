1c66915a3e112727439a5d5a0105e32b70804973
Robert Petz <rpetz@ignitelabs.net>
2025-05-08 16:05:20 -0700

    Tweaks to the structure of a universe

--- temporal/temporal.go
+++ temporal/temporal.go
@@ -3,18 +3,13 @@ package temporal
 import "github.com/ignite-laboratories/core"
 
 // Nexus is the global universe of all worldly dimensions.
-//
-// Abstractly, this represents: *Universe[string, World[string, []*Dimension[any, any]]]
-var Nexus = &Universe{
-	Worlds:   make(core.FilterableMap[string, World]),
-	Entities: make(core.FilterableMap[uint64, *core.Entity]),
-}
+var Nexus = make(Universe)
 
-// Universe is a collection of named worlds and their associated entities.
-type Universe struct {
-	Worlds   core.FilterableMap[string, World]
-	Entities core.FilterableMap[uint64, *core.Entity]
-}
+// Universe is a collection of named worlds.
+type Universe core.FilterableMap[string, World]
 
-// World is a collection of named dimensional slices.
-type World core.FilterableMap[string, core.FilterableSlice[*Dimension[any, any]]]
+// World is a collection of named dimensional slices and their associated entities.
+type World struct {
+	Dimensions core.FilterableMap[string, core.FilterableSlice[*Dimension[any, any]]]
+	Entities   core.FilterableMap[uint64, *core.Entity]
+}

564f0cb5051ae20129ec2b23ecae8bf300138081
Robert Petz <rpetz@ignitelabs.net>
2025-05-12 21:11:06 -0700

    Minor documentation clarification

--- colors/colors.go
+++ colors/colors.go
@@ -2,59 +2,59 @@ package colors
 
 import "github.com/ignite-laboratories/core/std"
 
-// Red represents the color 0xFF0000FF.
+// Red represents the RGBA color value 0xFF0000FF.
 var Red = std.RGBAFromHex(0xFF0000FF)
 
-// Green represents the color 0x00FF00FF.
+// Green represents the RGBA color value 0x00FF00FF.
 var Green = std.RGBAFromHex(0x00FF00FF)
 
-// Blue represents the color 0x0000FFFF.
+// Blue represents the RGBA color value 0x0000FFFF.
 var Blue = std.RGBAFromHex(0x0000FFFF)
 
-// White represents the color 0xFFFFFFFF.
+// White represents the RGBA color value 0xFFFFFFFF.
 var White = std.RGBAFromHex(0xFFFFFFFF)
 
-// Black represents the color 0x000000FF.
+// Black represents the RGBA color value 0x000000FF.
 var Black = std.RGBAFromHex(0x000000FF)
 
-// Grey11 represents the color 0x111111FF.
+// Grey11 represents the RGBA color value 0x111111FF.
 var Grey11 = std.RGBAFromHex(0x111111FF)
 
-// Grey22 represents the color 0x222222FF.
+// Grey22 represents the RGBA color value 0x222222FF.
 var Grey22 = std.RGBAFromHex(0x222222FF)
 
-// Grey33 represents the color 0x333333FF.
+// Grey33 represents the RGBA color value 0x333333FF.
 var Grey33 = std.RGBAFromHex(0x333333FF)
 
-// Grey44 represents the color 0x444444FF.
+// Grey44 represents the RGBA color value 0x444444FF.
 var Grey44 = std.RGBAFromHex(0x444444FF)
 
-// Grey55 represents the color 0x555555FF.
+// Grey55 represents the RGBA color value 0x555555FF.
 var Grey55 = std.RGBAFromHex(0x555555FF)
 
-// Grey66 represents the color 0x666666FF.
+// Grey66 represents the RGBA color value 0x666666FF.
 var Grey66 = std.RGBAFromHex(0x666666FF)
 
-// Grey77 represents the color 0x777777FF.
+// Grey77 represents the RGBA color value 0x777777FF.
 var Grey77 = std.RGBAFromHex(0x777777FF)
 
-// Grey88 represents the color 0x888888FF.
+// Grey88 represents the RGBA color value 0x888888FF.
 var Grey88 = std.RGBAFromHex(0x888888FF)
 
-// Grey99 represents the color 0x999999FF.
+// Grey99 represents the RGBA color value 0x999999FF.
 var Grey99 = std.RGBAFromHex(0x999999FF)
 
-// GreyAA represents the color 0xAAAAAAFF.
+// GreyAA represents the RGBA color value 0xAAAAAAFF.
 var GreyAA = std.RGBAFromHex(0xAAAAAAFF)
 
-// GreyBB represents the color 0xBBBBBBFF.
+// GreyBB represents the RGBA color value 0xBBBBBBFF.
 var GreyBB = std.RGBAFromHex(0xBBBBBBFF)
 
-// GreyCC represents the color 0xCCCCCCFF.
+// GreyCC represents the RGBA color value 0xCCCCCCFF.
 var GreyCC = std.RGBAFromHex(0xCCCCCCFF)
 
-// GreyDD represents the color 0xDDDDDDFF.
+// GreyDD represents the RGBA color value 0xDDDDDDFF.
 var GreyDD = std.RGBAFromHex(0xDDDDDDFF)
 
-// GreyEE represents the color 0xEEEEEEFF.
+// GreyEE represents the RGBA color value 0xEEEEEEFF.
 var GreyEE = std.RGBAFromHex(0xEEEEEEFF)

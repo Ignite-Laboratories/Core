eb5a527ae6e07cedb0304b4bad8c0f533abfa365
Robert Petz <rpetz@ignitelabs.net>
2025-09-13 16:29:48 -0700

    Update main.go

--- sys/netscape/git/main.go
+++ sys/netscape/git/main.go
@@ -3,5 +3,5 @@ package main
 import "git.ignitelabs.net/core/sys/netscape"
 
 func main() {
-	netscape.GitVanity.Navigate("https://github.com/ignite-laboratories", 8080)
+	netscape.GitVanity.Navigate("https://github.com/ignite-laboratories")
 }
